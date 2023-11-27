d7659974b0c871658c27e1542bfa2f71ba4a0768
NoemiP94 <m_emi94@hotmail.it>
2023-11-27 14:37:06 +0100

    second exercise

--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Main2 {
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Scrivi la prima parola");
+        String word1 = scanner.nextLine();
+        System.out.println("Scrivi la seconda parola");
+        String word2 = scanner.nextLine();
+        System.out.println("Scrivi la terza parola");
+        String word3 = scanner.nextLine();
+
+        String completeString = word1 + word2+ word3;
+        System.out.println("Il risultato è: " + completeString);
+
+        String reverseCompleteString = word3 + word2 + word1;
+        System.out.println("La stringa al contrario è : " + reverseCompleteString);
+
+
+
+    }
+
 }
